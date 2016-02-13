28a9eed0f5ff5a08765985f2f16118d2dcce2c0f
Jeff Clement <jsc@nddn.net>
2016-02-13 09:05:55 -0700

    Skeleton.

---
+++ server/CrmWrapper.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    public class CrmWrapper
+    {
+        private static CrmWrapper m_instance;
+        private IOrganizationService m_service;
+
+        public static void ConnectToMSCRM(string UserName, string Password, string SoapOrgServiceUri)
+        {
+        }
+
+
+        private CrmWrapper(string username, string password, string url)
+        {
+            ClientCredentials credentials = new ClientCredentials();
+            credentials.UserName.UserName = username;
+            credentials.UserName.Password = password;
+            Uri serviceUri = new Uri(url);
+            OrganizationServiceProxy proxy = new OrganizationServiceProxy(serviceUri, null, credentials, null);
+            proxy.EnableProxyTypes();
+            m_service = (IOrganizationService)proxy;
+        }
+
+        public static void Init(string username, string password, string url)
+        {
+            m_instance = new CrmWrapper(username, password, url);
+        }
+
+        public static CrmWrapper Instance { get { return m_instance; } }
+
+        public string Version
+        {
+            get
+            {
+                RetrieveVersionRequest versionRequest = new RetrieveVersionRequest();
+                RetrieveVersionResponse versionResponse = (RetrieveVersionResponse) m_service.Execute(versionRequest);
+                return versionResponse.Version;
+            }
+        }
+    }
+}
--- server/Program.cs
+++ server/Program.cs
@@ -1,15 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using JsonConfig;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using ServiceStack;
+using System;
+using System.ServiceModel.Description;
 
 namespace server
 {
+
+    [Route("/version")]
+    public class Version
+    {
+    }
+
+    [Route("/incident/{id}")]
+    public class Incident
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class IncidentResponse
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Owner { get; set; }
+    }
+
+    public class CRMService : Service
+    {
+        public object Any(Incident request)
+        {
+            return new IncidentResponse { Title = request.Id.ToString() };
+        }
+
+        public object Get(Version request)
+        {
+            return CrmWrapper.Instance.Version;
+        }
+    }
+
+    public class AppHost : AppSelfHostBase
+    {
+        public AppHost() : base("HttpListener Self-Host", typeof (CRMService).Assembly)
+        {
+        }
+
+        public override void Configure(Funq.Container container) { }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            CrmWrapper.Init(Config.MergedConfig.CrmUser, Config.MergedConfig.CrmPassword, Config.MergedConfig.CrmUrl);
+
+            var listeningOn = Config.MergedConfig.Listen;
+            var appHost = new AppHost()
+                .Init()
+                .Start(listeningOn);
+
+            Console.WriteLine("AppHost Created at {0}, listening on {1}", DateTime.Now, listeningOn);
+
+            Console.ReadKey();
         }
     }
 }
